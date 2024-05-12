85904e4149924fb12e85cff5d61dbcaa491ceb42
nanangsb <sefida.nanang@gmail.com>
2024-05-12 18:47:28 +0700

    Update index.js

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,12 @@
 import AppCard from "./AppCard";
 import BookmarkCard from "./BookmarkCard";
-import FeaturedNews from "./FeaturedNews";
+
 import PrayerTime from "./PrayerTime";
 
 const Home = () => {
   return (
     <div className="w-full flex flex-col gap-6">
-      <FeaturedNews />
+      
       <AppCard />
       <PrayerTime />
       <BookmarkCard />
